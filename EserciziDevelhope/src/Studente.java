f9f2825ed56035b7fccafd9fbe671a1803dfcb54
elianBertola <150374006+elianBertola@users.noreply.github.com>
2023-11-17 13:41:45 +0100

    Add files via upload

---
+++ EserciziDevelhope/src/Studente.java
@@ -0,0 +1,19 @@
+public class Studente{
+    private final String name;
+    private final String surname;
+    private final int id;
+    public Studente(String name, String surname, int id) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Studente{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
