cd58952737ba2645577c267e23f3133a1236b692
elianBertola <150374006+elianBertola@users.noreply.github.com>
2023-11-17 13:45:16 +0100

    Delete EserciziDevelhope directory

--- EserciziDevelhope/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Studente student1 = new Studente("Elian", "Bertola", 1);
-
-        System.out.println(student1);
-    }
-}
